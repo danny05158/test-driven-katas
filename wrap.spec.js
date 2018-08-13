bd0f38db8ec694e88e1d81f87f5dc55810562277
Daniel Casasola <casasolad86@gmail.com>
2018-08-13 10:49:40 -0500

    another test

--- wrap.spec.js
+++ wrap.spec.js
@@ -2,6 +2,8 @@ const { expect } = require('chai');
 const wrap = require('./wrap');
 
 describe('wrap', () => {
+  const string = 'Specifies the string which denotes the points at which each split should occur. The separator is treated as a string or as a regular expression. If a plain - text separator contains more than one character, that entire string must be found to represent a split point. If separator is omitted or does not occur in str, the array returned contains one element consisting of the entire string. If separator is an empty string, str is converted to an array of characters. If no separator is specified, an array containing the input string is returned.'
+
   it('Returns empty string if empty string was provided', () => {
     expect(wrap('', 10)).to.equal('');
   });
@@ -13,24 +15,21 @@ describe('wrap', () => {
   it('Splits strings into lines of no more than MaxLen', () => {
     const len = parseInt(Math.random() * 30 + 10, 10);
     expect(
-      wrap(
-        'Specifies the string which denotes the points at which each split should occur.The separator is treated as a string or as a regular expression.If a plain - text separator contains more than one character, that entire string must be found to represent a split point.If separator is omitted or does not occur in str, the array returned contains one element consisting of the entire string.If separator is an empty string, str is converted to an array of characters.If no separator is specified, an array containing the input string is returned.',
-        len
-      )
+      wrap(string, len)
         .split('\n')
         .every(str => str.length <= len)
     ).to.equal(true);
   });
 
-  it('Only splits lines at spaces', () => {
+  it('it only splits out word bounderies', () =>{
     const len = parseInt(Math.random() * 30 + 10, 10);
+    const wrapText = wrap(string,len)
+
     expect(
-      wrap(
-        'Specifies the string which denotes the points at which each split should occur.The separator is treated as a string or as a regular expression.If a plain - text separator contains more than one character, that entire string must be found to represent a split point.If separator is omitted or does not occur in str, the array returned contains one element consisting of the entire string.If separator is an empty string, str is converted to an array of characters.If no separator is specified, an array containing the input string is returned.',
-        len
-      )
-        .split('\n')
-        .every(str => str.findIndex(' ') !== 0 && str.findLastIndex !== len - 1)
-    ).to.equal(true);
-  });
+    string.split(' ').every(str => wrapText.includes(str))
+  ).to.equal(true);
+})
+
+
+
 });
